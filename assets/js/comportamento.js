dd85526f2faf583b04cbb621e54d04d8140de9b9
Alvaroifpi <alvarojosepassos@gmail.com>
2023-03-16 17:47:42 -0300

    Adição da validação para não aceitar valores nulos

--- assets/js/comportamento.js
+++ assets/js/comportamento.js
@@ -6,8 +6,8 @@ const cx_distancia = document.querySelector('#input_distancia')
 const btn_enviar = document.getElementById('btn_calcular')
 
 btn_enviar.addEventListener('click', () => {
-    nome_gasolina.innerText = "GASOLINA"
-    nome_alcool.innerText = "ALCOOL"
+    nome_gasolina.innerText = 'GASOLINA'
+    nome_alcool.innerText = 'ALCOOL'
 
     let preco_gasolina = Number(cx_preco_g.value)
     let preco_alcool = Number(cx_preco_a.value)
@@ -21,41 +21,45 @@ btn_enviar.addEventListener('click', () => {
     let gasto_total_gasolina = litros_gasolina * preco_gasolina
     let gasto_total_alcool = litros_alcool * preco_alcool
 
-    quant_g.innerText = `Você precisara de ${litros_gasolina.toFixed(2)} L`
-    quant_a.innerText = `Você precisara de ${litros_alcool.toFixed(2)} L`
-    preco_g.innerText = `Saira no total por ${gasto_total_gasolina.toFixed(2)} R$`
-    preco_a.innerText = `Saira no total por ${gasto_total_alcool.toFixed(2)} R$`
+    if (isNaN(litros_gasolina) || isNaN(litros_alcool) || isNaN(gasto_total_gasolina) || isNaN(gasto_total_alcool)) {
+        dica.innerText = 'Lembre-se de preencher todos os campos'
 
-    if (gasto_total_gasolina > dinheiro) {
-        let falt_g = (gasto_total_gasolina - dinheiro).toFixed(2)
-        suficiente_g.innerText = `Você não tem o suficiente para viajar falta ${falt_g} R$`
-    }
-    else if (gasto_total_gasolina <= dinheiro) {
-        let sobra_g = (dinheiro - gasto_total_gasolina).toFixed(2)
-        suficiente_g.innerText = `Você tem o suficiente para viajar e ira sobrar ${sobra_g} R$`
-    }
+    } else {
+        quant_g.innerText = `Você precisara de ${litros_gasolina.toFixed(2)} L`
+        quant_a.innerText = `Você precisara de ${litros_alcool.toFixed(2)} L`
+        preco_g.innerText = `Saira no total por ${gasto_total_gasolina.toFixed(2)} R$`
+        preco_a.innerText = `Saira no total por ${gasto_total_alcool.toFixed(2)} R$`
 
-    if (gasto_total_alcool > dinheiro) {
-        let falt_a = (gasto_total_alcool - dinheiro).toFixed(2)
-        suficiente_a.innerText = `Você não tem o suficiente para viajar falta ${falt_a} R$`
-    }
-    else if (gasto_total_alcool <= dinheiro) {
-        let sobra_a = (dinheiro - gasto_total_alcool).toFixed(2)
-        suficiente_a.innerText = `Você tem o suficiente para viajar e ira sobrar ${sobra_a} R$`
-    }
+        if (gasto_total_gasolina > dinheiro) {
+            let falt_g = (gasto_total_gasolina - dinheiro).toFixed(2)
+            suficiente_g.innerText = `Você não tem o suficiente para viajar falta ${falt_g} R$`
+        }
+        else if (gasto_total_gasolina <= dinheiro) {
+            let sobra_g = (dinheiro - gasto_total_gasolina).toFixed(2)
+            suficiente_g.innerText = `Você tem o suficiente para viajar e ira sobrar ${sobra_g} R$`
+        }
 
-    if (gasto_total_alcool > gasto_total_gasolina) {
-        red = (gasto_total_alcool - gasto_total_gasolina).toFixed(2)
-        dica.innerText = `Recomendo você viajar com gasolina pois economizará R$ ${red} em relação ao outro combustível`
-    }
+        if (gasto_total_alcool > dinheiro) {
+            let falt_a = (gasto_total_alcool - dinheiro).toFixed(2)
+            suficiente_a.innerText = `Você não tem o suficiente para viajar falta ${falt_a} R$`
+        }
+        else if (gasto_total_alcool <= dinheiro) {
+            let sobra_a = (dinheiro - gasto_total_alcool).toFixed(2)
+            suficiente_a.innerText = `Você tem o suficiente para viajar e ira sobrar ${sobra_a} R$`
+        }
 
-    else if (gasto_total_alcool < gasto_total_gasolina) {
-        red = (gasto_total_gasolina - gasto_total_alcool).toFixed(2)
-        dica.innerText = `Recomendo você viajar com alcool pois economizará R$ ${red} em relação ao outro combustível`
-    }
+        if (gasto_total_alcool > gasto_total_gasolina) {
+            red = (gasto_total_alcool - gasto_total_gasolina).toFixed(2)
+            dica.innerText = `Recomendo você viajar com gasolina pois economizará R$ ${red} em relação ao outro combustível`
+        }
 
-    else if (gasto_total_alcool == gasto_total_gasolina) {
-        dica.innerText = `Recomendo você viajar com qualquer um dos combustivel pois eles terão o mesmo custo`
-    }
+        else if (gasto_total_alcool < gasto_total_gasolina) {
+            red = (gasto_total_gasolina - gasto_total_alcool).toFixed(2)
+            dica.innerText = `Recomendo você viajar com alcool pois economizará R$ ${red} em relação ao outro combustível`
+        }
 
+        else if (gasto_total_alcool == gasto_total_gasolina) {
+            dica.innerText = `Recomendo você viajar com qualquer um dos combustivel pois eles terão o mesmo custo`
+        }
+    }
 })
